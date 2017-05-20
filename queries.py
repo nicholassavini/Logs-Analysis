1c520f825b83ef9a2b2b96e152eed986cb52003d
vagrant <vagrant@vagrant.vm>
2017-05-20 16:29:46 +0000

    Create basic function to return top articles

--- queries.py
+++ queries.py
@@ -4,3 +4,18 @@ import bleach
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=news")
+
+def top_articles():
+	conn = connect()
+	curs = conn.cursor()
+	curs.execute("""
+ 			 	SELECT title, COUNT(title) as views from articles_with_logs
+ 			 	GROUP BY title ORDER BY views DESC
+ 			 	""")
+	articles = curs.fetchall()
+	conn.commit()
+	conn.close()
+
+	return articles 
+
+print(top_articles())
