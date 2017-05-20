db6e1432d3ae9bf53e46ac368601852550c555c5
vagrant <vagrant@vagrant.vm>
2017-05-20 17:39:35 +0000

    Add log_errors view

--- views.sql
+++ views.sql
@@ -16,3 +16,12 @@ CREATE VIEW articles_with_logs AS
 	FROM articles_with_authors
 	INNER JOIN log
 	ON log.path=articles_with_authors.new_slug;
+
+CREATE VIEW log_errors AS
+	SELECT  
+	TO_CHAR(time, 'MM/DD/YYYY') as date, 
+	CAST(SUM(CASE WHEN status='404 NOT FOUND' THEN 1 ELSE 0 END) AS real) / 
+	CAST(COUNT(status) AS real) * 100 AS error_percentage
+	FROM log 
+	GROUP BY date
+	ORDER BY date ASC;
