f2d9d34214b458a053d934589ab50f0198c05600
vagrant <vagrant@vagrant.vm>
2017-05-20 16:29:14 +0000

    Add view to combine aritcles with logs

--- views.sql
+++ views.sql
@@ -8,3 +8,11 @@ CREATE VIEW articles_with_authors AS
 	FROM articles 
 	INNER JOIN authors 
 	ON authors.id=articles.author;
+
+CREATE VIEW articles_with_logs AS
+	SELECT
+	title, 
+	name
+	FROM articles_with_authors
+	INNER JOIN log
+	ON log.path=articles_with_authors.new_slug;
