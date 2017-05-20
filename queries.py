62de0e351563ae8197d6637258f2a162c3f3c25d
vagrant <vagrant@vagrant.vm>
2017-05-20 16:56:28 +0000

    Change top_titles function
    Make tweaks to the function so that it now accepts input, and can return
    both the top titles and the top authors.

--- queries.py
+++ queries.py
@@ -5,17 +5,18 @@ def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=news")
 
-def top_articles():
+def top_items(item):
 	conn = connect()
 	curs = conn.cursor()
 	curs.execute("""
- 			 	SELECT title, COUNT(title) as views from articles_with_logs
- 			 	GROUP BY title ORDER BY views DESC
- 			 	""")
+ 			 	SELECT {0}, COUNT({0}) as views from articles_with_logs
+ 			 	GROUP BY {0} ORDER BY views DESC
+ 			 	""".format(item))
 	articles = curs.fetchall()
 	conn.commit()
 	conn.close()
 
 	return articles 
 
-print(top_articles())
+print(top_items("title"))
+print(top_items("name"))
